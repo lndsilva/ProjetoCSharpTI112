ce851ea795045f693bf360f5a9c8446c79d6adb0
lndsilva <lndsilva@hotmail.com>
2024-09-18 08:08:53 -0300

    projeto multjogos

--- MultJogos/Program.cs
+++ MultJogos/Program.cs
@@ -16,7 +16,7 @@ namespace MultJogos
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmSobreNos());
+            Application.Run(new frmSplash());
         }
     }
 }
